f7af5f60c430d7c9992ac7713a840982353de05c
JenniferFulton <jennifer.reeves8@gmail.com>
2022-09-16 13:27:33 -0400

    known function created, however I would like a default setting. Next step is to create a validation

--- cheatle/__main__.py
+++ cheatle/__main__.py
@@ -1,2 +1,12 @@
+from email.policy import default
+import click
+
+#function that takes in the known/green values
+#would like to set a default value to "_____" but when I do that within the click option "Green Letters:  [_____]:" is displayed as the prompt
+@click.command()
+@click.option('--known', prompt = "Green Letters: ", help='Letters that are in the correct position. Use a "_" for letters that are not in the right position')
+def known_word(known):
+    click.echo(known)
+
 if __name__ == '__main__':
-    print("todo")
+    known_word()
