31db648f3e3c99d10aaaba52edd0df1ed838ed3d
JenniferFulton <jennifer.reeves8@gmail.com>
2022-09-20 18:06:27 -0400

    else statement added to validator

--- cheatle/__main__.py
+++ cheatle/__main__.py
@@ -6,7 +6,8 @@ def validate_input(ctx, known, value):
     if re.search('^[a-zA-Z_]{5}$', value) == None:
         raise click.BadParameter("Please only use characters and '_' for known value and make sure it is 5 characters long")
 
-    return value
+    else:
+        return value
 
 #would like to set a default value of "_____"
 @click.command()
