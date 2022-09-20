14e44959b32730c462771c857ddd4847b8b06cac
JenniferFulton <jennifer.reeves8@gmail.com>
2022-09-20 18:04:54 -0400

    able to run known variable through the validator and return known if there is no error

--- cheatle/__main__.py
+++ cheatle/__main__.py
@@ -1,15 +1,14 @@
 import click
+import re
 
 #validate user input for known
 def validate_input(ctx, known, value):
-    #check to make sure input is 5 chracters
-    if len(value) != 5:
-        click.echo(value)
-        raise click.BadParameter("Must be atleast 5 characters")
+    if re.search('^[a-zA-Z_]{5}$', value) == None:
+        raise click.BadParameter("Please only use characters and '_' for known value and make sure it is 5 characters long")
 
     return value
 
-#would like to set a default value
+#would like to set a default value of "_____"
 @click.command()
 @click.option('--known', prompt=True, type=click.UNPROCESSED, callback=validate_input,)
 def cheatle(known):
