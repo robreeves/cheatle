085ae5001379da97eefba2aca9745a1b68a825ab
JenniferFulton <jennifer.reeves8@gmail.com>
2022-09-20 17:57:01 -0400

    able to validate if user input is atleast 5 characters for known value

--- cheatle/__main__.py
+++ cheatle/__main__.py
@@ -1,9 +1,13 @@
 import click
 
-#validate uuser input
+#validate user input for known
 def validate_input(ctx, known, value):
     #check to make sure input is 5 chracters
-    click.echo(type(known))
+    if len(value) != 5:
+        click.echo(value)
+        raise click.BadParameter("Must be atleast 5 characters")
+
+    return value
 
 #would like to set a default value
 @click.command()
